46826d6a54cd32c9f59eb716cf6f5a944d3b4656
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-12-30 01:42:33 +0200

    categories

---
+++ ReactQuiz/src/constans/categories.js
@@ -0,0 +1,22 @@
+import {faPalette, faTheatreMasks, faLandmark} from "@fontawesome/free-solid-svg-icons";
+
+const CATEGORIES = {
+    art: {
+        name: 'Мистецтво',
+        icon: faPalette,
+        color: 'green',
+    },
+
+    theatre: {
+        name: 'Театр',
+        icon: faTheatreMasks,
+        color: 'green',
+    },
+
+    history: {
+        name: 'Історія',
+        icon: faLandmark,
+        color: 'green',
+    }
+
+}
